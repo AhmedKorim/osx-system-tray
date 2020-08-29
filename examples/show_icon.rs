27e5cedbfd4e46fde2ebad1650f718ac1ea174c0
ahmed korim <ahmedkorrim@gmail.com>
2020-08-29 22:49:19 +0200

    life times error

--- examples/show_icon.rs
+++ examples/show_icon.rs
@@ -1,14 +1,19 @@
 use std::thread;
 use std::time::Duration;
 
-use osx_system_tray::OsxSystemTray;
+use osx_system_tray::{OsxSystemTray,OsxSystemTrayEvent};
 
 pub fn main() {
 	const ICON_BUFFER: &'static [u8] = include_bytes!("rust-logo.png");
 	let mut osx_tray = OsxSystemTray::new();
 	osx_tray.set_tray_icon_from_buffer(ICON_BUFFER);
+	let rx = osx_tray.handler.clone();
+	let join = thread::spawn(move || {
+		loop {
+			dbg!("looping over");
+			rx.send(OsxSystemTrayEvent::ChangeImage(ICON_BUFFER));
+			thread::sleep(Duration::from_millis(1000));
+		}
+	});
 	osx_tray.run();
-	loop {
-		thread::sleep(Duration::from_millis(100));
-	}
 }
